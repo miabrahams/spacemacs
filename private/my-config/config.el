62921b1248b9d7c65007c0acf0a7aa3ac78d577b
Michael <miabraha@gmail.com>
2018-04-28 00:25:51 -0400

    Remove old unused stuff from my-config including save-visited-files

--- private/my-config/config.el
+++ private/my-config/config.el
@@ -89,20 +89,15 @@
   (setq-default znc-servers '(("scorpio" 6667 nil
                                ((abrahams "abrahams" "alphAnum8ric"))))))
 
+(spacemacs|use-package-add-hook "org-agenda"
 
-(spacemacs|use-package-add-hook "org"
-  ;; Emacs doesn't understand org-protocol:// filenames without this.
-  ;; Note: I deleted org-protocol.el/elc in my c:\bin\emacs installation.
-  ;; Not sure this is necessary.
-
-  ;; This works for me:
-  ;; C:\bin\emacs\bin\emacsclientw.exe "org-protocol:/capture?template=p&link=aaaa&title=TITLE&body=BODY"
   :pre-init
-  (require 'org-protocol)
-  ;; (package-initialize)
-  ;; http://www.howardism.org/Technical/Emacs/journaling-org.html
-  ;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
-  ;; For Chrome extension :D
+  (defun my-daily-org-agenda ()
+    (interactive)
+    "Daily org agenda"
+    (org-agenda nil "d"))
+
+  (evil-leader/set-key "oa" 'my-daily-org-agenda)
 
 
   :post-config
@@ -116,39 +111,6 @@ currently open, based on `org-agenda-files'."
         (when (member (buffer-file-name)
                       (mapcar 'expand-file-name (org-agenda-files t)))
           (save-buffer)))))
-
-  (defadvice org-capture
-      (after make-full-window-frame activate)
-    "Advise capture to be the only window when used as a popup"
-    (if (equal "emacs-capture" (frame-parameter nil 'name))
-        (delete-other-windows)))
-  (defadvice org-capture-finalize
-      (after delete-capture-frame activate)
-    "Advise capture-finalize to close the frame"
-    (if (equal "emacs-capture" (frame-parameter nil 'name))
-        (delete-frame))
-    (org-save-all-org-buffers))
-  (defun my-cmp-todo-state (a b)
-    "Compare the todo states of strings A and B."
-    (let* ((ma (or (get-text-property 1 'org-marker a)
-                   (get-text-property 1 'org-hd-marker a)))
-           (mb (or (get-text-property 1 'org-marker b)
-                   (get-text-property 1 'org-hd-marker b)))
-           (fa (and ma (marker-buffer ma)))
-           (fb (and mb (marker-buffer mb)))
-           (todo-kwds
-            (or (and fa (with-current-buffer fa org-todo-keywords-1))
-                (and fb (with-current-buffer fb org-todo-keywords-1))))
-           (ta (or (get-text-property 1 'todo-state a) ""))
-           (tb (or (get-text-property 1 'todo-state b) ""))
-           (la (- (length (member ta todo-kwds))))
-           (lb (- (length (member tb todo-kwds))))
-           (donepa (member ta org-done-keywords-for-agenda))
-           (donepb (member tb org-done-keywords-for-agenda)))
-      (cond ((and donepa (not donepb)) -1)
-            ((and (not donepa) donepb) +1)
-            (nil)
-            )))
 
   (defun org-agenda-reschedule-to-today ()
     (interactive)
@@ -199,25 +161,12 @@ currently open, based on `org-agenda-files'."
                                        (concat dropbox-directory "text/org/todo.org")
                                        (concat dropbox-directory "text/work/work.org")
                                        (concat dropbox-directory "text/creative/artwork.org"))
-                org-confirm-elisp-link-not-regexp "org-journal-new-entry"
-                org-directory (concat dropbox-directory "/text")
-                org-default-notes-file (concat org-directory "/notes.org")
                 org-habit-graph-column 52
                 org-habit-show-all-today t
                 org-habit-show-done-always-green t
                 org-habit-show-habits-only-for-today t
-                org-habit-today-glyph 32
-                org-extend-today-until 1
-                org-todo-keywords
-                '((sequence "TODO(t)" "|" "DONE(d)")
-                  (sequence "UNREAD(u)" "BLANK(b)" "DRAFT(r)" "SENT(s)"))
-                org-link-frame-setup
-                '((vm . vm-visit-folder-other-frame)
-                  (vm-imap . vm-visit-imap-folder-other-frame)
-                  (gnus . org-gnus-no-new-news)
-                  (file . find-file-other-window)
-                  (wl . wl-other-frame))
-                )
+                org-habit-today-glyph 32)
+
 
   ;; From Aaron Bieber
   (defun air-org-skip-subtree-if-priority (priority)
@@ -246,11 +195,88 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
                       (org-agenda-overriding-header "Unscheduled tasks"))))
            )))
 
-  (defun my-daily-org-agenda ()
-    (interactive)
-    "Daily org agenda"
-    (org-agenda nil "d"))
-  (evil-leader/set-key "oa" 'my-daily-org-agenda)
+
+  (defun my-org-agenda-mode-hook ()
+    "Set up org-agenda mode"
+    (spacemacs/disable-hl-line-mode)
+    (setq-local truncate-lines 1)
+    (defun org-agenda-capture (&optional with-time)
+      "Call `org-capture' with the date at point.
+With a `C-1' prefix, use the HH:MM value at point (if any) or the
+current HH:MM time."
+      (interactive "P")
+      (if (not (eq major-mode 'org-agenda-mode))
+          (user-error "You cannot do this outside of agenda buffers")
+        (call-interactively 'org-capture)))
+    )
+  (add-hook 'org-agenda-mode-hook 'my-org-agenda-mode-hook)
+
+  )
+
+
+
+(spacemacs|use-package-add-hook "org"
+  :pre-init
+  ;; Org-protocol: disabled for now.
+  ;; Test using this command:
+  ;; C:\bin\emacs\bin\emacsclientw.exe "org-protocol:/capture?template=p&link=aaaa&title=TITLE&body=BODY"
+  ;; I deleted org-protocol.el/elc in c:\bin\emacs; not sure this is necessary.
+  ;; (require 'org-protocol)
+
+  ;; (package-initialize)
+  ;; http://www.howardism.org/Technical/Emacs/journaling-org.html
+  ;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
+  ;; For Chrome extension :D
+  :post-config
+  (defadvice org-capture
+      (after make-full-window-frame activate)
+    "Advise capture to be the only window when used as a popup"
+    (if (equal "emacs-capture" (frame-parameter nil 'name))
+        (delete-other-windows)))
+  (defadvice org-capture-finalize
+      (after delete-capture-frame activate)
+    "Advise capture-finalize to close the frame"
+    (if (equal "emacs-capture" (frame-parameter nil 'name))
+        (delete-frame))
+    (org-save-all-org-buffers))
+  (defun my-cmp-todo-state (a b)
+    "Compare the todo states of strings A and B."
+    (let* ((ma (or (get-text-property 1 'org-marker a)
+                   (get-text-property 1 'org-hd-marker a)))
+           (mb (or (get-text-property 1 'org-marker b)
+                   (get-text-property 1 'org-hd-marker b)))
+           (fa (and ma (marker-buffer ma)))
+           (fb (and mb (marker-buffer mb)))
+           (todo-kwds
+            (or (and fa (with-current-buffer fa org-todo-keywords-1))
+                (and fb (with-current-buffer fb org-todo-keywords-1))))
+           (ta (or (get-text-property 1 'todo-state a) ""))
+           (tb (or (get-text-property 1 'todo-state b) ""))
+           (la (- (length (member ta todo-kwds))))
+           (lb (- (length (member tb todo-kwds))))
+           (donepa (member ta org-done-keywords-for-agenda))
+           (donepb (member tb org-done-keywords-for-agenda)))
+      (cond ((and donepa (not donepb)) -1)
+            ((and (not donepa) donepb) +1)
+            (nil)
+            )))
+
+
+  (setq-default org-confirm-elisp-link-not-regexp "org-journal-new-entry"
+                org-directory (concat dropbox-directory "/text")
+                org-default-notes-file (concat org-directory "/notes.org")
+                org-extend-today-until 1
+                org-todo-keywords
+                '((sequence "TODO(t)" "|" "DONE(d)")
+                  (sequence "UNREAD(u)" "BLANK(b)" "DRAFT(r)" "SENT(s)"))
+                org-link-frame-setup
+                '((vm . vm-visit-folder-other-frame)
+                  (vm-imap . vm-visit-imap-folder-other-frame)
+                  ;; (gnus . org-gnus-no-new-news)
+                  (file . find-file-other-window)
+                  (wl . wl-other-frame))
+                )
+
 
   (add-to-list 'org-modules 'org-habit)
 
@@ -306,22 +332,10 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
     (set-face-attribute 'org-todo nil :background nil)
     (set-face-attribute 'org-habit-alert-future-face nil :inherit 'org-habit-alert-face)
     (set-face-attribute 'org-habit-overdue-future-face nil :inherit 'org-habit-overdue-face)
-    (defun org-agenda-capture (&optional with-time)
-      "Call `org-capture' with the date at point.
-With a `C-1' prefix, use the HH:MM value at point (if any) or the
-current HH:MM time."
-      (interactive "P")
-      (if (not (eq major-mode 'org-agenda-mode))
-          (user-error "You cannot do this outside of agenda buffers")
-        (call-interactively 'org-capture))))
+    )
 
   (add-hook 'org-mode-hook 'my-org-mode-hook)
 
-  (defun my-org-agenda-mode-hook ()
-    "Set up org-agenda mode"
-    (spacemacs/disable-hl-line-mode)
-    (setq-local truncate-lines 1))
-  (add-hook 'org-agenda-mode-hook 'my-org-agenda-mode-hook)
 
   (define-key org-mode-map [(control tab)] nil)
   (define-key org-mode-map (kbd "C-c d") #'org-schedule)
--- private/my-config/packages.el
+++ private/my-config/packages.el
@@ -9,51 +9,30 @@
 
 ;;; Code:
 
+
 (setq-default my-config-packages
-    '(
-      ;; package my-configs go here
-
-      ninja-mode
-      profiler
-      ;; general-close   ; Wait until available in MELPA
-      ;; flycheck-tip
-      systemd
-      save-visited-files
-      ;; org-journal
-      cmake-project
-      dired+
-      avy-zap
-      ;; back-button
-      ;; highlight-tail
-      ;; counsel ;Check this out
-      ;; cmake-ide
-      ;; apropospriate-theme
-      ;; bliss-theme
-      ;; lush-theme
-      ;; cmake-font-lock
-
-      comment-dwim-2
-      ;; dired-single
-      ;; bookmark+
-      ;; corral
-      howdoi
-      help-mode+
-      ;; olivetti
-      ;; org2blog
-      ;; rtags
-
-      ;; filesets
-      ;; helm-filesets
-      ;; filesets+
-      ;; synonyms
-
-      windresize
-      hideshow
-      pos-tip
-      persistent-scratch
-      julia-mode
-      julia-shell
-      ))
+              '(
+                ;; package my-configs go here
+                ninja-mode
+                profiler
+                systemd
+                ;; save-visited-files
+                ;; cmake-project
+                ;; dired+
+                ;; avy-zap
+                ;; cmake-ide
+                ;; cmake-font-lock
+                comment-dwim-2
+                howdoi
+                ;; help-mode+
+                olivetti
+                ;; org2blog
+                ;; rtags
+                ;; synonyms
+                persistent-scratch
+                ;; julia-mode
+                ;; julia-shell
+                ))
 
 
 ;; List of packages to exclude.
@@ -63,99 +42,26 @@
                 helm-company
                 ))
 
-(defun my-config/init-julia-shell ()
-  (use-package julia-shell :ensure t :defer t))
-
-(defun my-config/init-julia-mode ()
-  (use-package julia-mode
-    :ensure t
+(defun my-config/init-ninja-mode ()
+  (use-package "ninja-mode"
     :defer t
-    :config
-    (defun my-julia-mode-hooks ()
-      (require 'julia-shell-mode))
-    (add-hook 'julia-mode-hook 'my-julia-mode-hooks)
-    (define-key julia-mode-map (kbd "C-c C-c") 'julia-shell-run-region-or-line)
-    (define-key julia-mode-map (kbd "C-c C-s") 'julia-shell-save-and-go)
+    :commands 'ninja-mode
     ))
 
-
-
-
-(defun my-config/init-hideshow ()
-  (use-package "hideshow"
-    :ensure t
-    :commands (hs-minor-mode hs-hide-all hs-show-all)
-    :init
-    (add-hook 'prog-mode-hook (lambda () (hs-minor-mode 1)))
-    ))
-
-(defun my-config/init-olivetti ()
-  (use-package "olivetti"
-    :ensure t
-    :commands (olivetti-mode)
-    ))
-
-(defun my-config/init-org2blog ()
-  (use-package "org2blog"
-    :ensure t
-    :commands (org2blog/wp-login)
-    :init
-    (setq org2blog/wp-blog-alist
-          '(("Risk and Time"
-             :url "http://riskandtime.net/xmlrpc.php"
-             :username "abrahams"
-             :password "WRlecS%FVNDQ%HT67p"
-             :default-title "New Post"
-             :default-categories ("Finance")
-             :tags-as-categories nil)
-            ))
-    ))
-
-(defun my-config/init-persistent-scratch ()
-  (use-package "persistent-scratch"
-    :ensure t
-    :init
-    (setq-default persistent-scratch-save-file "~/.emacs.d/.cache/.persistent-scratch")
-    (persistent-scratch-setup-default)
-    ))
-
-(defun my-config/init-pos-tip ()
-  (use-package "pos-tip"
-    :ensure t
-    :commands (pos-tip-show)
-    :init
-    (defun describe-in-popup (fn)
-      "Open a postip containing the help text of `symbol-at-point' using FN.
-FN should be either `describe-variable' or `describe-function'."
-      (let* ((thing (symbol-at-point))
-             (description
-              (replace-regexp-in-string
-               "\\[BACK\\]" ""
-               (save-window-excursion
-                 (funcall fn thing)
-                 (switch-to-buffer "*Help*")
-                 (buffer-string)))))
-        ;; (pos-tip-show description 'popup-tip-face nil nil -1)
-        (pos-tip-show description nil nil nil -1)
-        )))
-  )
-
-
-(defun my-config/init-windresize ()
-  (use-package "windresize"
-    :ensure t
-    :commands (windresize)
-    :init
-    (global-set-key (kbd "C-x ^") 'windresize)
-    ))
-
-
-
+(defun my-config/init-profiler ()
+  (use-package "profiler" :defer t))
 
 (defun my-config/init-systemd ()
   (use-package "systemd"
-    :ensure t
+    :defer t
     :commands 'systemd-mode))
+
+
+;; (defun my-config/init-save-visited-files ()
+;;   (use-package "save-visited-files"
+;;     :ensure t
+;;     :config
+;;     (turn-on-save-visited-files-mode)))
 
 
 (defun my-config/init-cmake-project ()
@@ -167,12 +73,121 @@ FN should be either `describe-variable' or `describe-function'."
     (add-hook 'c-mode-hook 'maybe-cmake-project-hook)
     (add-hook 'c++-mode-hook 'maybe-cmake-project-hook)))
 
+(defun my-config/init-avy-zap ()
+  (use-package "avy-zap"
+    :ensure t
+    :commands 'avy-zap-up-to-char-dwim
+    :init
+    (global-set-key (kbd "M-z") #'avy-zap-up-to-char-dwim)))
+
 (defun my-config/init-comment-dwim-2 ()
   (use-package "comment-dwim-2"
     :ensure t
     :commands 'comment-dwim-2
     :config
     (global-set-key (kbd "M-;") 'comment-dwim-2)))
+
+(defun my-config/init-howdoi ()
+  (use-package "howdoi"
+    :ensure t
+    :commands 'howdoi-query
+    :config
+    (define-key help-map (kbd "?") 'howdoi-query)))
+
+(defun my-config/init-persistent-scratch ()
+  (use-package "persistent-scratch"
+    :ensure t
+    :init
+    (setq-default persistent-scratch-save-file "~/.emacs.d/.cache/.persistent-scratch")
+    (persistent-scratch-setup-default)
+    ))
+
+
+(defun my-config/init-olivetti ()
+  (use-package "olivetti"
+    :ensure t
+    :commands (olivetti-mode)
+    ))
+
+
+;; (defun my-config/init-julia-shell ()
+;;   (use-package julia-shell
+;;     :ensure t
+;;     :defer t))
+
+;; (defun my-config/init-julia-mode ()
+;;   (use-package julia-mode
+;;     :ensure t
+;;     :defer t
+;;     :config
+;;     (defun my-julia-mode-hooks ()
+;;       (require 'julia-shell-mode))
+;;     (add-hook 'julia-mode-hook 'my-julia-mode-hooks)
+;;     (define-key julia-mode-map (kbd "C-c C-c") 'julia-shell-run-region-or-line)
+;;     (define-key julia-mode-map (kbd "C-c C-s") 'julia-shell-save-and-go)
+;;     ))
+
+
+
+
+
+;; (defun my-config/init-windresize ()
+;;   (use-package "windresize"
+;;     :ensure t
+;;     :commands (windresize)
+;;     :init
+;;     (global-set-key (kbd "C-x ^") 'windresize)
+;;     ))
+
+
+
+;; (defun my-config/init-org2blog ()
+;;   (use-package "org2blog"
+;;     :ensure t
+;;     :commands (org2blog/wp-login)
+;;     :init
+;;     (setq org2blog/wp-blog-alist
+;;           '(("Risk and Time"
+;;              :url "http://riskandtime.net/xmlrpc.php"
+;;              :username "abrahams"
+;;              :password "WRlecS%FVNDQ%HT67p"
+;;              :default-title "New Post"
+;;              :default-categories ("Finance")
+;;              :tags-as-categories nil)
+;;             ))
+;;     ))
+
+
+
+;; (defun my-config/init-rtags ()
+;;   (if ( spacemacs/system-is-linux )
+;;       (use-package "rtags"
+;;         :ensure t
+;;         :commands 'rtags-mode
+;;         :init
+;;         (setq-default rtags-autostart-diagnostics t
+;;                       rtags-completions-enabled t)
+;;         (defun my-rtags-setup ()
+;;           (flycheck-select-checker 'rtags)
+;;           (rtags-mode))
+;;         (add-hook 'c-mode-common-hook #'my-rtags-setup)
+;;         :config
+;;         ;; c-mode-common-hook is also called by c++-mode
+;;         (push 'company-rtags company-backends)
+;;         (define-key c-mode-base-map (kbd "M-.")   #'rtags-find-symbol-at-point)
+;;         (define-key c-mode-base-map (kbd "M-,")   #'rtags-find-references-at-point)
+;;         (define-key c-mode-base-map (kbd "C-M-.") #'rtags-find-file)
+;;         (define-key c-mode-base-map (kbd "C-.")   #'rtags-find-symbol)
+;;         (define-key c-mode-base-map (kbd "C-,")   #'rtags-find-references)
+;;         (define-key c-mode-base-map (kbd "C-<")   #'rtags-find-virtuals-at-point)
+;;         (define-key c-mode-base-map (kbd "C-M-;") #'rtags-imenu)
+;;         (define-key c-mode-base-map (kbd "M-'")   #'company-complete)
+;;         ;; THIS IS AN AMAZING BUFFER, PLEASE GIVE IT LOVE
+;;         (define-key c-mode-base-map (kbd "C-M-?") #'rtags-diagnostics)
+;;         )))
+
+
+
 
 ;; (defun my-config/init-synonyms ()
 ;;   (use-package "synonyms"
@@ -184,32 +199,6 @@ FN should be either `describe-variable' or `describe-function'."
 ;;     ;; Define a key here later
 ;;     ))
 
-(defun my-config/init-howdoi ()
-  (use-package "howdoi"
-    :ensure t
-    :commands 'howdoi-query
-    :config
-    (define-key help-map (kbd "?") 'howdoi-query)))
-
-
-
-;; (defun my-config/init-general-close ()
-;;   (use-package "general-close" :ensure t
-;;     :config
-;;     (global-set-key (kbd "M-)") #'general-close)
-;;     ))
-
-
-;; Sadly not working  :c
-(defun my-config/init-back-button ()
-  (if spacemacs/system-is-mswindows
-      (use-package "back-button"
-        :ensure t
-        :init
-        (back-button-mode 1)
-        (global-set-key (kbd "<M-left>")  #'back-button-local-backward)
-        (global-set-key (kbd "<M-right>") #'back-button-local-forward)
-        )))
 
 ;; (defun my-config/init-cmake-font-lock ()
 ;;   (use-package "cmake-font-lock"
@@ -221,148 +210,3 @@ FN should be either `describe-variable' or `describe-function'."
 ;;     (cmake-font-lock-set-signature "my_warn" (:warning))
 ;;     (add-to-list '(:warning . font-lock-warning-face) cmake-font-lock-argument-kind-face-alist)
 ;; ))
-
-
-
-;; Spacemacs/use-package macro
-;; (defconst spacemacs-use-package-font-lock-keywords
-;;   '(("(\\(spacemacs/\\[use|init\\]-package\\)\\_>[ \t']*\\(\\(?:\\sw\\|\\s_\\)+\\)?"
-;;      (1 font-lock-keyword-face)
-;;      (2 font-lock-constant-face nil t))))
-;; (font-lock-add-keywords 'emacs-lisp-mode spacemacs-use-package-font-lock-keywords)
-
-
-;; (defun configuration-layer//load-layers-files (layers files)
-;;   "Load the files of list FILES for all passed LAYERS."
-;;   (dolist (layer layers)
-;;     (configuration-layer//load-layer-files layer files)))
-
-;; (defvar spacemacs-layername "spacemacs")
-
-;; (defmacro spacemacs/use-package (package-name &rest ARGS)
-;;   (let ((package-string (if (stringp package-name)
-;;                             package-name
-;;                           (symbol-name package-name)))
-;;         (layer-string   (if (boundp 'spacemacs-layername)
-;;                             (if (stringp spacemacs-layername)
-;;                                 spacemacs-layername
-;;                               (symbol-name spacemacs-layername))
-;;                           "spacemacs")))
-;;     (if ARGS
-;;         `(progn (defun ,(intern (concat layer-string "/init-" package-string)) ()
-;;                   (use-package ,package-string ,@ARGS)))
-;;       `(progn (defun ,(intern (concat layer-string "/init-" package-string)) ()
-;;                 (use-package ,package-string))))))
-
-;; (spacemacs/use-package "corral"
-;;                        :ensure t
-;;                        :config
-;;                        (global-set-key (kbd "M-9") #'corral-parentheses-backward)
-;;                        (global-set-key (kbd "M-0") #'corral-parentheses-forward)
-;;                        (global-set-key (kbd "M-[") #'corral-brackets-backward)
-;;                        (global-set-key (kbd "M-]") #'corral-brackets-forward)
-;;                        (global-set-key (kbd "M-\"") #'corral-double-quotes-backward)
-;;                        (setq-default corral-preserve-point t))
-
-
-
-(defun my-config/init-rtags ()
-  (if ( spacemacs/system-is-linux )
-      (use-package "rtags"
-        :ensure t
-        :commands 'rtags-mode
-        :init
-        (setq-default rtags-autostart-diagnostics t
-                      rtags-completions-enabled t)
-        (defun my-rtags-setup ()
-          (flycheck-select-checker 'rtags)
-          (rtags-mode))
-        (add-hook 'c-mode-common-hook #'my-rtags-setup)
-
-        :config
-        ;; c-mode-common-hook is also called by c++-mode
-        (push 'company-rtags company-backends)
-
-        (define-key c-mode-base-map (kbd "M-.")   #'rtags-find-symbol-at-point)
-        (define-key c-mode-base-map (kbd "M-,")   #'rtags-find-references-at-point)
-        (define-key c-mode-base-map (kbd "C-M-.") #'rtags-find-file)
-        (define-key c-mode-base-map (kbd "C-.")   #'rtags-find-symbol)
-        (define-key c-mode-base-map (kbd "C-,")   #'rtags-find-references)
-        (define-key c-mode-base-map (kbd "C-<")   #'rtags-find-virtuals-at-point)
-        (define-key c-mode-base-map (kbd "C-M-;") #'rtags-imenu)
-        (define-key c-mode-base-map (kbd "M-'")   #'company-complete)
-
-        ;; THIS IS AN AMAZING BUFFER, PLEASE GIVE IT LOVE
-        (define-key c-mode-base-map (kbd "C-M-?") #'rtags-diagnostics)
-        )))
-
-
-(defun my-config/init-flycheck-tip ()
-  (use-package "flycheck-tip"
-    :ensure t
-    :config
-    (eval-after-load 'flycheck
-      (progn
-        (require 'flycheck-tip)
-        (flycheck-tip-use-timer 'verbose)
-        (define-key prog-mode-map (kbd "C-c C-n") 'flycheck-tip-cycle)))))
-
-(defun my-config/init-save-visited-files ()
-  (use-package "save-visited-files"
-    :ensure t
-    :config
-    (turn-on-save-visited-files-mode)))
-
-(defun my-config/init-ninja-mode ()
-  (use-package "ninja-mode"
-    :ensure t
-    :commands 'ninja-mode
-    ))
-
-(defun my-config/init-profiler ()
-  (use-package "profiler" :defer t))
-
-(defun my-config/init-avy-zap ()
-  (use-package "avy-zap"
-    :ensure t
-    :commands 'avy-zap-up-to-char-dwim
-    :init
-    (global-set-key (kbd "M-z") #'avy-zap-up-to-char-dwim)))
-
-
-;; (defun my-config/init-apropospriate-theme ()
-;;   (use-package "apropospriate-theme" :ensure t))
-
-
-;; (defun my-config/init-bliss-theme ()
-;;   (use-package "bliss-theme" :ensure t))
-
-
-;; (defun my-config/init-highlight-tail ()
-;;   (use-package "highlight-tail"
-;;     :ensure t
-;;     :config
-;;     (highlight-tail-mode)
-;;     ;; Define a key here later
-;;     ))
-
-;; (defun my-config/helm-gtags-define-keys-for-mode (mode)
-;;   "Define key bindings for the specific MODE."
-;;   (when (fboundp mode)
-;;     (let ((hook (intern (concat (symbol-name mode) "-hook"))))
-;;       (add-hook hook 'helm-gtags-mode))
-;;     (evil-leader/set-key-for-mode mode
-;;       "mgc" 'helm-gtags-create-tags
-;;       "mgd" 'helm-gtags-find-tag
-;;       "mgf" 'helm-gtags-select-path
-;;       "mgg" 'helm-gtags-dwim
-;;       "mgG" 'helm-gtags-dwim-other-window
-;;       "mgi" 'helm-gtags-tags-in-this-function
-;;       "mgl" 'helm-gtags-parse-file
-;;       "mgn" 'helm-gtags-next-history
-;;       "mgp" 'helm-gtags-previous-history
-;;       "mgr" 'helm-gtags-find-rtag
-;;       "mgR" 'helm-gtags-resume
-;;       "mgs" 'helm-gtags-select
-;;       "mgS" 'helm-gtags-show-stack
-;;       "mgu" 'helm-gtags-update-tags)))
